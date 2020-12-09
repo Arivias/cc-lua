65416ce69874a5783007d933f54f8c8e63d53f7d
Sidewinder 7 <jpfleck@gmail.com>
2020-12-08 17:54:01 -0800

    fixed potential corruption and redid branch

--- locust/installer.lua
+++ locust/installer.lua
@@ -1,11 +1,13 @@
 local args = {...}
 local branch = "main"
+local do_install = true
 
 local function install(branch, remote, filename)
 	if not filename then filename = remote end
 	print("fetching "..filename.."...")
 	local handle = fs.open("disk/"..filename,"w")
 	local r = http.get("https://raw.githubusercontent.com/Arivias/cc-lua/"..branch.."/locust/"..remote)
+	print(r.getResponseCode())
 	handle.write(r.readAll())
 	r.close()
 	handle.close()
@@ -13,18 +15,28 @@ end
 
 if #args > 0 then
 	if args[1] == "update" then
+		do_install = false
 		print("Fetching latest version...")
 		local remote = http.get("https://raw.githubusercontent.com/Arivias/cc-lua/main/locust/installer.lua")
 		local h = fs.open("locust-installer.lua","w")
-		h.write(remote.readAll())
-		remote.close()
-		h.close()
-		print("Done!")
-	elseif args[1] == "branch" or args[1] == "-b"
+		r = remote.readAll()
+		if #r > 10 then
+			h.write(r)
+			remote.close()
+			h.close()
+			print("Done!")
+		else
+			print("Something went wrong. Aborting update.")
+		end
+	elseif args[1] == "branch" or args[1] == "-b" then
 		print("Fetching branch: "..args[2])
 		branch = args[2]
+	else
+		print("Usage: locust-installer [update] [-b branchname]")
 	end
-else
+end
+
+if do_install then
 	print("Installing locust to disk...")
 	install(branch, "locust.lua", "startup.lua")
 	install(branch, "dispatcher.lua")
