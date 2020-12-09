39b029b2032df842f21c3854ba4fdb2287a43c08
lenarwhalrus <jpwright4600@csu.fullerton.edu>
2020-12-08 17:06:30 -0800

    adjusted for clarity

--- locust/dispatcher.lua
+++ locust/dispatcher.lua
@@ -4,31 +4,33 @@ end
 
 config = fs.open("disk/cfg","r")
 
-y_pos = readNum(config)
-x_offset = readNum(config)
-y_offset = readNum(config)
-z_offset = readNum(config)
-rows = readNum(config)
-seg_size = readNum(config)
-seg_count = readNum(config)
+y_spawn = readNum(config) -- global y of
+
+x_grid = readNum(config) -- grid corner coords
+y_grid = readNum(config)
+z_grid = readNum(config)
+
+d_grid = readNum(config) -- (y) depth of desired area (should be negative)
+w_grid = readNum(config) -- (z) width of desired area
+l_grid = readNum(config) -- (x) length of desired area
 
 config.close()
 
-for row = 1,rows do
+-- write a list of dig spots in the grid area
+for row = 1,l_grid+1 do
+	for col = 1,w_grid+1 do
 
-	for seg = seg_count,1,-1 do
-
-		x = x_offset + (seg-1)*4*seg_size + row%2*2
-		z = z_offset + (row - 1) * 2
+		x = x_grid + 4*col + (2*(row - 1)%5)
+		z = z_grid + col-1
 
 		h = fs.open("job","w")
-		
+
 		h.writeLine(tostring(x))
-		h.writeLine(tostring(y_offset))
+		h.writeLine(tostring(y_grid))
 		h.writeLine(tostring(z))
-		
-		h.writeLine(tostring(seg_size))
-		h.writeLine(tostring(y_pos))
+
+		h.writeLine(tostring(d_grid))
+		h.writeLine(tostring(y_spawn))
 
 		h.close()
 
@@ -37,5 +39,4 @@ for row = 1,rows do
 		shell.run("mv ../job ../disk/job")
 
 	end
-
 end
